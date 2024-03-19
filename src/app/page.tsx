3200bdc83286ae823107467c53fa50500f62caaf
“elstua” <stua359@gmail.com>
2024-03-19 18:15:14 +0000

    Start to develop it

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p>Artem's portfolio under construction. Until it'll be ready, you can find me on:
+          <a href="mailto:stua@fastmail.com"> stua@fastmail.com</a>
+          </p>
+      </div>
+    </main>
+  );
+}
