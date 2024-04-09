8f67a39e02752915f1317435bcb7a0f28bee33ca
“elstua” <stua359@gmail.com>
2024-04-09 16:28:35 +0100

    new favicons and images for socials

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <html lang="en" className="scroll-smooth">
+    <html lang="en">
+    <SpeedInsights/>
     <App />
     </html>
   </React.StrictMode>
